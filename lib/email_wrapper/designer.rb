0aaadd2632e9e46e06b1b827e0bf1e834c7e63c6
Tobi Oduah <tobi.oduah@andela.com>
2016-04-27 12:14:14 +0100

    Reordered template files and moved sendgrid setup to production config

--- lib/email_wrapper/designer.rb
+++ lib/email_wrapper/designer.rb
@@ -3,7 +3,7 @@ module EmailWrapper
     class << self
 
       def inserted_content(content)
-        template_file = File.read(Rails.root.join("lib/email_wrapper/template.htm.erb"))
+        template_file = File.read(Rails.root.join("lib/email_wrapper/templates/template.htm.erb"))
         ERB.new(template_file).result(OpenStruct.new(content).instance_eval { binding })
       end
 
