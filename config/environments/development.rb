1af4e7ce1d53905350c09b456ccd2e9df5ceb458
Tobi Oduah <tobi.oduah@andela.com>
2016-06-15 15:00:44 +0100

    status with successful mails sent

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.logger = nil
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "localhost:3000" }
