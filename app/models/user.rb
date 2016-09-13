27f7f0f0863a8aef7c52da42aaa90b858988a173
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2016-09-13 22:01:21 +0100

    fixed issues with users not being able to authorize

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_one :preference, dependent: :destroy
-  has_paper_trail
+  # has_paper_trail
 
   EMAIL_FORMAT= /(?<email>[.\w-]+@andela).co[m]?\z/
   after_update :notify_of_changes, if: :slack_name_changed?
